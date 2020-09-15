1f4ad88a3d41095aaf9cc176c9b5493d7f6f2b50
iamsteven526 <r08942052@ntu.edu.tw>
2020-09-15 13:39:51 +0800

    add2qpskscma+code

--- SCMA/2qpskscma+code/simul.m
+++ SCMA/2qpskscma+code/simul.m
@@ -2,7 +2,7 @@
 % Faculty of Radio Engineering
 % Department of Theoretical Fundamentals of Radio Engineering
 % Vyacheslav P. Klimentyev and Alexander B. Sergienko, 2015
-% 00 01 10 11
+
 % Codebooks
 clc;
 clear;
@@ -35,6 +35,42 @@ CB(:,:,6) = [ 0                  0                  0                  0;...
               0.7071             1j*0.7071          -1j*0.7071         -0.7071;...
               0.7071             1j*0.7071          -1j*0.7071         -0.7071;...
               0                  0                  0                  0 ];
+          
+CB(:,:,7) = [ 0                  0                  0                  0;...
+             0.7071             1j*0.7071          -1j*0.7071         -0.7071;...
+              0                  0                  0                  0;...
+              0.7071             1j*0.7071          -1j*0.7071         -0.7071 ];
+
+CB(:,:,8) = [ 0.7071             1j*0.7071          -1j*0.7071         -0.7071;...
+              0                  0                  0                  0;...
+             0.7071             1j*0.7071          -1j*0.7071         -0.7071;...
+              0                  0                  0                  0 ];
+
+CB(:,:,9) = [0.7071             1j*0.7071          -1j*0.7071         -0.7071;...
+             0.7071             1j*0.7071          -1j*0.7071         -0.7071;...
+              0                  0                  0                  0;...
+              0                  0                  0                  0 ];
+
+CB(:,:,10) = [ 0                  0                  0                  0;...
+              0                  0                  0                  0;...
+              0.7071             1j*0.7071          -1j*0.7071         -0.7071;...
+             0.7071             1j*0.7071          -1j*0.7071         -0.7071 ];
+
+CB(:,:,11) = [0.7071             1j*0.7071          -1j*0.7071         -0.7071;...
+              0                  0                  0                  0;...
+              0                  0                  0                  0;...
+             0.7071             1j*0.7071          -1j*0.7071         -0.7071 ];
+
+CB(:,:,12) = [ 0                  0                  0                  0;...
+              0.7071             1j*0.7071          -1j*0.7071         -0.7071;...
+              0.7071             1j*0.7071          -1j*0.7071         -0.7071;...
+              0                  0                  0                  0 ];
+         
+          
+
+          
+
+
 
 K = size(CB, 1); % number of orthogonal resources
 M = size(CB, 2); % number of codewords in each codebook
@@ -49,24 +85,27 @@ Nerr  = zeros(V, length(SNR));
 Nbits = zeros(V, length(SNR));
 BER   = zeros(V, length(SNR));
 
-maxNumErrs = 1000;
-maxNumBits = 5e7;
-Niter      = 10;
+maxNumErrs = 100;
+maxNumBits = 1e6;
+Niter      = 5;
 ldpcDecoder = comm.LDPCDecoder;
 ldpcEncoder = comm.LDPCEncoder;
+
 for k = 1:length(SNR)
 
     N0 = 1/(10^(SNR(k)/10)); % noise power
 
     while ((min(Nerr(:,k)) < maxNumErrs) && (Nbits(1,k) < maxNumBits))
-
-        %x = randi([0 M-1], V, N/2); % log2(M)-bit symbols
-        dam = randi([0 1], V, N/2); %32400
+        display(k);
+        display(Nbits(1,k));
+        %x = randi([0 M-1], V, N); % log2(M)-bit symbols
+        dam = randi([0 1], V, N/2); %32400      
         for pp = 1:V
            w(pp,:) = ldpcEncoder(dam(pp,:)');%64800
         end
+        
         %h = 1/sqrt(2)*(randn(K, V, N)+1j*randn(K, V, N)); % Rayleigh channel
-        h = 1/sqrt(2)*(repmat(randn(1, V, N), K, 1)+1j*repmat(randn(1, V, N), K, 1)); % no diversity
+        h = 1/sqrt(2)*(repmat(randn(1, V, N), K, 1)+1j*repmat(randn(1, V, N), K, 1));
         for pp = 1:1
             for qq = 1:N/1
                 h(:,:,(pp-1)*N/1+qq) = h(:,:,(pp-1)*N/1+1);
@@ -84,30 +123,23 @@ for k = 1:length(SNR)
 
         % symbol to bit conversion
         %r    = de2bi(x, log2(M), 'left-msb');
-        %data = zeros(log2(M)*N/2, V);
-        %for kk = 1:V
-        %    data(:,kk) = reshape(downsample(r, V, kk-1).',[],1);
-        %end
-
-        % LLR to bit conversion
-        %datadec = reshape((LLR <= 0), [log2(M) N/2*V]).';
-        datar   = zeros(log2(M)*N/2, V);
+        datar = zeros(log2(M)*N/2, V);
         for kk = 1:V
             for tt = 1:N/2
                 datar(2*tt-1,kk) = LLR(2*kk-1,tt)*N0/1;
                 datar(2*tt,kk) = LLR(2*kk,tt)*N0/1;
-            end%datar(:,kk) = reshape(downsample(datadec, V, kk-1).', [], 1);
+            end
         end
         for pp = 1:V
             ansbit(pp,:) = ldpcDecoder(datar(:,pp));
         end
+
         err        = sum(xor(dam', ansbit'));
         Nerr(:,k)  = Nerr(:,k) + err.';
-        Nbits(:,k) = Nbits(:,k) + log2(M)*N/4;
-        k
-        display(sum(Nerr(:,k))/6)
+        Nbits(:,k) = Nbits(:,k) + log2(M)*N;
     end
     BER(:,k) = Nerr(:,k)./Nbits(:,k);
-    
+    k
+    display(sum(Nerr(:,k))/6)
 end
 plot(EbN0,log10(BER))

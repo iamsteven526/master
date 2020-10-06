5584db26437650431fa3f35fa20cbcfa2102668c
iamsteven526 <r08942052@ntu.edu.tw>
2020-10-07 01:04:19 +0800

    1010

--- SCMA/coded_scma/simulation.m
+++ SCMA/coded_scma/simulation.m
@@ -40,18 +40,18 @@ K = size(CB, 1); % number of orthogonal resources
 M = size(CB, 2); % number of codewords in each codebook
 V = size(CB, 3); % number of users (layers)
 
-N = 64800; % SCMA signals in frame
-
-EbN0 = 20:5:40;
-SNR  = EbN0 + 10*log10(log2(M)*V/K);   %noise power maybe wrong!!!
+N = 1024; % SCMA signals in frame
+R = 0.5;
+EbN0 = 30:5:40;
+SNR  = EbN0 + 10*log10(R*log2(M)*V/K);   %noise power maybe wrong!!!
 
 Nerr  = zeros(V, length(SNR));
 Nbits = zeros(V, length(SNR));
 BER   = zeros(V, length(SNR));
 
 maxNumErrs = 1000;
-maxNumBits = 5e7;
-Niter      = 10;
+maxNumBits = 1e7;
+Niter      = 8;
 ldpcDecoder = comm.LDPCDecoder;
 ldpcEncoder = comm.LDPCEncoder;
 for k = 1:length(SNR)
@@ -61,9 +61,12 @@ for k = 1:length(SNR)
     while ((min(Nerr(:,k)) < maxNumErrs) && (Nbits(1,k) < maxNumBits))
 
         %x = randi([0 M-1], V, N/2); % log2(M)-bit symbols
+        display(k);
+        display(Nbits(1,k))
         dam = randi([0 1], V, N/2); %32400
         for pp = 1:V
-           w(pp,:) = ldpcEncoder(dam(pp,:)');%64800
+           %w(pp,:) = ldpcEncoder(dam(pp,:)');%64800
+           w(pp,:) = nrPolarEncode(dam(pp,:)',N,10,false);
         end
         %h = 1/sqrt(2)*(randn(K, V, N)+1j*randn(K, V, N)); % Rayleigh channel
         h = 1/sqrt(2)*(repmat(randn(1, V, N), K, 1)+1j*repmat(randn(1, V, N), K, 1)); % no diversity
@@ -81,7 +84,8 @@ for k = 1:length(SNR)
         y = awgn(s, SNR(k));
 
         LLR = scmadec(y, CB, h, N0, Niter);
-
+        LLR(LLR==inf) = 1500;
+        LLR(LLR==-inf) = -1500;
         % symbol to bit conversion
         %r    = de2bi(x, log2(M), 'left-msb');
         %data = zeros(log2(M)*N/2, V);
@@ -99,15 +103,15 @@ for k = 1:length(SNR)
             end%datar(:,kk) = reshape(downsample(datadec, V, kk-1).', [], 1);
         end
         for pp = 1:V
-            ansbit(pp,:) = ldpcDecoder(datar(:,pp));
+            ansbit(pp,:) = nrPolarDecode(datar(:,pp),N/2,N,8,10,false,24);
+            %ansbit(pp,:) = ldpcDecoder(datar(:,pp));
         end
         err        = sum(xor(dam', ansbit'));
         Nerr(:,k)  = Nerr(:,k) + err.';
-        Nbits(:,k) = Nbits(:,k) + log2(M)*N/4;
-        k
-        display(sum(Nerr(:,k))/6)
+        Nbits(:,k) = Nbits(:,k) + log2(M)*N*R;
     end
     BER(:,k) = Nerr(:,k)./Nbits(:,k);
+    display(sum(Nerr(:,k))/6)
     
 end
 plot(EbN0,log10(BER))

598b3a9b73d78d3e3f663dcf02f495f043ba2580
iamsteven526 <r08942052@ntu.edu.tw>
2020-12-02 18:23:31 +0800

    dddd

--- GDMA_alamouti/GDMA_alamouti/parameters.h
+++ GDMA_alamouti/GDMA_alamouti/parameters.h
@@ -12,9 +12,9 @@
 #define		NUMERIC_LIMIT			1e-100
 #define		LLR_LIMIT				1000
 
-#define		BLOCK_NUM				500000											// number of blocks to be simulated
+#define		BLOCK_NUM				5000000											// number of blocks to be simulated
 
-#define		SNR_NUM					9													// number of SNR points to be simulated
+#define		SNR_NUM					12													// number of SNR points to be simulated
 #define		SNR_START				0 													// in dB
 #define		SNR_STEP				5													// in dB
 

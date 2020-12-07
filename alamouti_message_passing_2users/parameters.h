8ee5930153ac953362062f1af63372c0eb93bcfe
iamsteven526 <r08942052@ntu.edu.tw>
2020-12-07 21:55:12 +0800

    d

--- alamouti_message_passing_2users/parameters.h
+++ alamouti_message_passing_2users/parameters.h
@@ -9,13 +9,13 @@
 
 //---------- simulator ----------
 
-#define		NUMERIC_LIMIT			1e-100
+#define		NUMERIC_LIMIT			1e-30
 #define		LLR_LIMIT				1000
 
 #define		BLOCK_NUM				500000000											// number of blocks to be simulated
 
 #define		SNR_NUM					9													// number of SNR points to be simulated
-#define		SNR_START				0 													// in dB
+#define		SNR_START				20 													// in dB
 #define		SNR_STEP				5													// in dB
 
 #define		HARD(x)					( (x) > 0 ? 0 : 1 )

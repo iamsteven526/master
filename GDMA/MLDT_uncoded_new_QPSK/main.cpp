d77a098b99a5537966c7286fbf35a028694f02a7
iamsteven526 <r08942052@ntu.edu.tw>
2020-12-31 01:00:33 +0800

    asdfadsf

---
+++ GDMA/MLDT_uncoded_new_QPSK/main.cpp
@@ -0,0 +1,279 @@
+#include <iostream>
+#include "parameters.h"
+#include <cmath>
+using namespace std;
+
+#pragma warning(disable:4996)
+int main()
+{
+	//---------- check for overflow ----------
+	if ((long double)NUM_USER*BLOCK_NUM*BLOCK_LEN < 0)
+	{
+		cout << "OVERFLOW" << endl;
+		system("pause");
+		return 0;
+	}
+	//---------- memory allocation ----------
+	int **data = new int*[NUM_USER];
+	double **app = new double*[2 * BLOCK_LEN];
+	double **appLlr = new double *[NUM_USER];
+	double **chCoef = new double *[NUM_USER];
+	for (int i = 0; i < NUM_USER; i++)
+	{
+		data[i] = new int[BLOCK_LEN];
+		appLlr[i] = new double[2 * BLOCK_LEN];
+		chCoef[i] = new double[4];
+	}
+	for (int i = 0; i < 2 * BLOCK_LEN; i++)
+		app[i] = new double[NUM_LEVEL];
+
+	double** tx = new double* [NUM_USER];
+	double *preTx = nullptr, **rx = nullptr, **postRx = nullptr, *txFilter = nullptr, *rxFilter = nullptr, *prePilot=nullptr, ** pilot=nullptr;
+	if (SYNCHRONOUS)
+	{
+		for (int i = 0; i < NUM_USER; i++)
+		{
+			tx[i] = new double[2 * BLOCK_LEN];
+		}
+		rx = new double *[2 * BLOCK_LEN];
+		for (int i = 0; i < 2 * BLOCK_LEN; i++)
+		{
+			rx[i] = new double[2]; // real and imaginary
+		}
+	}
+	else
+	{
+		pilot = new double* [NUM_USER];
+		for (int i = 0; i < NUM_USER; i++)
+		{
+			tx[i] = new double[BLOCK_LEN*UP_RATE*SPREAD_LEN];
+			pilot[i] = new double[BLOCK_LEN*UP_RATE*SPREAD_LEN];
+		}
+		preTx = new double[BLOCK_LEN*UP_RATE*SPREAD_LEN];
+		prePilot = new double[BLOCK_LEN*UP_RATE*SPREAD_LEN];
+		rx = new double *[BLOCK_LEN*UP_RATE*SPREAD_LEN];
+		postRx = new double*[BLOCK_LEN*UP_RATE*SPREAD_LEN];
+		for (int i = 0; i < BLOCK_LEN*UP_RATE*SPREAD_LEN; i++)
+		{
+			rx[i] = new double[2]; // real and imaginary
+			postRx[i] = new double[2]; // real and imaginary
+		}
+		txFilter = new double[(2 * TRUNCATION + 1)*UP_RATE];
+		rxFilter = new double[(2 * TRUNCATION + 1)*UP_RATE];
+		SRRCGeneration(rxFilter, 0);
+	}
+	int *groupSize = nullptr, **group = nullptr;
+	double *variation = nullptr, *distList = nullptr, **centroid = nullptr, **estimate = nullptr, **softAssign = nullptr;
+	if (CE_SCHEME == 0)
+	{
+		groupSize = new int[GROUP_SIZE];
+		variation = new double[GROUP_SIZE];
+		distList = new double[2 * BLOCK_LEN];
+		group = new int*[GROUP_SIZE];
+		centroid = new double *[GROUP_SIZE];
+		for (int i = 0; i < GROUP_SIZE; i++)
+		{
+			group[i] = new int[2 * BLOCK_LEN];
+			centroid[i] = new double[2]; // real and imaginary
+		}
+		estimate = new double *[NUM_USER];
+		for (int i = 0; i < NUM_USER; i++)
+		{
+			estimate[i] = new double[2]; // real and imaginary
+		}
+		if (EM_GMM)
+		{
+			softAssign = new double*[2 * BLOCK_LEN];
+			for (int i = 0; i < 2 * BLOCK_LEN; i++)
+			{
+				softAssign[i] = new double[GROUP_SIZE];
+			}
+		}
+	}
+
+	int* known_drift = nullptr;
+	//if (COLLISION == 0)
+	known_drift = new int[2 * NUM_USER];
+
+	int ***trellis = nullptr;
+	double **alpha = nullptr, **beta = nullptr, ***gamma = nullptr;
+	if (DIFF_ENC && (DIFF_RX_SCHEME == 1))
+	{
+		alpha = new double*[sizeof(double)*(BLOCK_LEN + 1)];
+		beta = new double*[sizeof(double)*(BLOCK_LEN + 1)];
+		for (int i = 0; i < BLOCK_LEN + 1; i++)
+		{
+			alpha[i] = new double[2];
+			beta[i] = new double[2];
+		}
+		gamma = new double **[BLOCK_LEN];
+		for (int i = 0; i < BLOCK_LEN; i++)
+		{
+			gamma[i] = new double*[2];
+			for (int j = 0; j < 2; j++)
+			{
+				gamma[i][j] = new double[2];
+			}
+		}
+		trellis = new int**[2];
+		for (int i = 0; i < 2; i++)
+		{
+			trellis[i] = new int*[2];
+			for (int j = 0; j < 2; j++)
+			{
+				trellis[i][j] = new int[2];
+			}
+		}
+		TrellisConstruction(trellis);
+	}
+	double ber[SNR_NUM],bler[SNR_NUM], mse[SNR_NUM] = { 0 }, mse_centroid[SNR_NUM] = { 0 };
+	long double itCount[SNR_NUM] = { 0 };
+	FILE *result_txt = fopen("result.txt", "w");
+	//---------- declaration ----------
+	printf("GDMA-BPSK system\n");
+	printf("Number of users: %d\n\n", NUM_USER);
+	fprintf(result_txt, "GDMA-BPSK system\n");
+	fprintf(result_txt, "Number of users: %d\n\n", NUM_USER);
+	if (CE_SCHEME == 1)
+	{
+		printf("Perfect CSI\n\n");
+		fprintf(result_txt, "Perfect CSI\n\n");
+	}
+	else
+	{
+		if (PROPOSAL == 0)
+		{
+			printf("Clustering method : original\n");
+			fprintf(result_txt, "Clustering method : original\n\n");
+		}
+		else if (PROPOSAL == 1)
+		{
+			printf("Clustering method : propose-1\n");
+			fprintf(result_txt, "Clustering method : propose-1\n\n");
+		}
+		else
+		{
+			printf("Clustering method : propose-2\n");
+			fprintf(result_txt, "Clustering method : propose-2\n\n");
+		}
+		if (INI_METHOD == 0)
+		{
+			printf("Cluster-based channel estimation: LBG algo.\n");
+			fprintf(result_txt, "Cluster-based channel estimation: LBG algo.\n");
+		}
+		if (INI_METHOD == 1)
+		{
+			printf("Cluster-based channel estimation: k-means++\n");
+			fprintf(result_txt, "Cluster-based channel estimation: k-means++\n");
+		}
+		if (INI_METHOD == 2)
+		{
+			printf("Cluster-based channel estimation: modified k-means++\n");
+			fprintf(result_txt, "Cluster-based channel estimation: modified k-means++\n");
+		}
+		if (EM_GMM)
+		{
+			printf("w/ Gaussian mixture model\n");
+			fprintf(result_txt, "w/ Gaussian mixture model\n");
+		}
+		printf("\n"); fprintf(result_txt, "\n");
+	}
+	printf("Block length: %d\n\n", BLOCK_LEN);
+	fprintf(result_txt, "Block length: %d\n\n", BLOCK_LEN);
+	if (DIFF_ENC)
+	{
+		printf("Differential encoding,\n");
+		fprintf(result_txt, "Differential encoding,\n");
+	}
+	if (DIFF_ENC && (DIFF_RX_SCHEME == 1))
+	{
+		printf("w/ soft demapping\n\n");
+		fprintf(result_txt, "w/ soft demapping\n\n");
+	}
+	if (DIFF_ENC && (DIFF_RX_SCHEME == 0))
+	{
+		printf("w/ differential decoding\n\n");
+		fprintf(result_txt, "w/ differential decoding\n\n");
+	}
+	if (SYNCHRONOUS)
+	{
+		printf("Synchronous system\n\n");
+		fprintf(result_txt, "Synchronous system\n\n");
+	}
+	else
+	{
+		printf("Asynchronous system\n");
+		printf("Drifting range: [%.3f, %.3f]\n\n", -0.5*DRIFT_RANGE, 0.5*DRIFT_RANGE);
+		fprintf(result_txt, "Asynchronous system\n");
+		fprintf(result_txt, "Drifting range: [%.3f, %.3f]\n\n", -0.5*DRIFT_RANGE, 0.5*DRIFT_RANGE);
+	}
+	printf("-\n\n"); fprintf(result_txt, "-\n\n");
+	//---------- simulation process ----------
+	for (int i = 0; i < SNR_NUM; i++)
+	{
+		double snrdB = SNR_START + (double)i * SNR_STEP;
+		double snr = pow(10., snrdB / 10.);
+		double variance = 0.5 / snr;
+		long double error = 0;
+		printf("SNR[dB] = %.1f\n", snrdB);
+		int block = 1;
+		for (block; block <= BLOCK_NUM; block++)
+		{
+			
+			//cout << "meow" << endl;
+			Transmitter(data, preTx, tx, txFilter,rxFilter,pilot,prePilot, known_drift);
+			//Transmitter(data, preTx, tx, txFilter,pilot);
+			EnergyProfile(chCoef);
+			MultipleAccessChannel(sqrt(variance), chCoef, tx, rx, pilot,known_drift);
+			if (!SYNCHRONOUS) ReceivingFilter(rx, postRx, rxFilter, txFilter, pilot,chCoef);
+			//system("pause");
+			if (CE_SCHEME == 0) // cluster-based channel estimation
+			{
+				Clustering(rx, centroid, group, groupSize, distList, variation, softAssign, variance, estimate, itCount[i],chCoef,known_drift);
+				MSEComparison(chCoef, estimate, mse[i],rx,centroid,known_drift); // user specification
+				//CentroidMSEComparison(chCoef, estimate, mse_centroid[i], rx, centroid);
+			}
+			MLDT(variance, chCoef, rx, app, appLlr, estimate,known_drift);
+			int BLERa = error;
+			Detector(data, appLlr, trellis, alpha, beta, gamma, error,known_drift);
+			int BLERb = error;
+            if(BLERa != BLERb){
+				bler[i]++;
+			}
+			ber[i] = error / ((long double)NUM_USER * block * (BLOCK_LEN - DIFF_ENC)); // excluding reference symbol
+			//cout << itCount[i] << " ";
+			if (CE_SCHEME == 1) printf("Block# = %d, BER = %e\r", block, ber[i]); 
+			else printf("Block# = %d, BER = %e,BLER = %e, MSE = %e, MSE_centroid = %e,iteration = %e\r", block, ber[i],(bler[i]/block), mse[i] / (double)(NUM_USER * block), mse_centroid[i] / (double)(NUM_LEVEL * block), itCount[i]/(double)(block));
+			
+			if (error > 10000 && block>10000)
+				break;
+		}
+		mse[i] /= (double)(NUM_USER* block);
+		itCount[i] /= (double)(NUM_USER * block);
+		if (CE_SCHEME == 1) fprintf(result_txt, "SNR[dB] = %.1f, BER = %e\n", snrdB, ber[i]); 
+		else fprintf(result_txt, "SNR[dB] = %.1f, BER = %e, MSE = %e, MSE_centroid = %e\n", snrdB, ber[i], mse[i], mse_centroid[i]);
+		printf("\n\n");
+	}
+	fprintf(result_txt, "\n\n");
+	for (int i = 0; i < SNR_NUM; i++)
+	{
+		fprintf(result_txt, "%e, ", ber[i]);
+	}
+	fprintf(result_txt, "\n");
+	if (CE_SCHEME == 0)
+	{
+		for (int i = 0; i < SNR_NUM; i++)
+		{
+			fprintf(result_txt, "%e, ", mse[i]);
+		}
+		fprintf(result_txt, "\n");
+		for (int i = 0; i < SNR_NUM; i++)
+		{
+			fprintf(result_txt, "%f, ", itCount[i]);
+		}
+		fprintf(result_txt, "\n");
+	}
+	fclose(result_txt);
+	//system("pause");
+	return 0;
+}
---
+++ GDMA/MLDT_uncoded_new_QPSK/mldt.cpp
@@ -0,0 +1,266 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include "parameters.h"
+#include <cmath>
+using namespace std;
+
+void MLDT(double variance, double **chCoef, double **rx, double **app, double **appLlr, double **estimate, int *known_drift)
+{
+	int count = 0;
+	if (COLLISION)
+	{
+		for (int i = 0; i < BLOCK_LEN; i++)
+		{
+			while (true)
+			{
+				bool flag = true;
+				//---------- APPs ----------
+				for (int num_level = 0; num_level < NUM_LEVEL; num_level++)
+				{
+					int reg = num_level;
+					double estimate_sum = 0;
+					for (int j = NUM_USER - 1; j >= 0; j--)
+					{
+						if (CE_SCHEME == 1)
+							estimate_sum += pow(-1, (reg % 2)) * chCoef[j][0];
+						else
+							estimate_sum += pow(-1, (reg % 2)) * estimate[j][0];
+
+						reg /= 2;
+					}
+					app[i][num_level] = exp(-pow(rx[i][0] - estimate_sum, 2) / (2. * (variance + count * MODIFIER)));
+				}
+
+				if (MODIFIED_LLR)
+				{
+					for (int j = 0; j < NUM_LEVEL; j++)
+					{
+						if (app[i][j] == 0)
+						{
+							flag = false;
+							break;
+						}
+					}
+					if (!flag)
+					{
+						count++;
+						continue;
+					}
+				}
+				//---------- normalization ----------
+				double temp = 0;
+				for (int j = 0; j < NUM_LEVEL; j++)
+				{
+					if (app[i][j] < NUMERIC_LIMIT) app[i][j] = NUMERIC_LIMIT;
+					temp += app[i][j];
+				}
+				for (int j = 0; j < NUM_LEVEL; j++)
+				{
+					app[i][j] /= temp;
+					if (app[i][j] < NUMERIC_LIMIT) app[i][j] = NUMERIC_LIMIT;
+				}
+				//---------- APPs ----------
+				for (int num_level = 0; num_level < NUM_LEVEL; num_level++)
+				{
+					int reg = num_level;
+					double estimate_sum = 0;
+					for (int j = NUM_USER - 1; j >= 0; j--)
+					{
+						if (CE_SCHEME == 1)
+							estimate_sum += pow(-1, (reg % 2)) * chCoef[j][1];
+
+						else
+							estimate_sum += pow(-1, (reg % 2)) * estimate[j][1];
+
+						reg /= 2;
+					}
+					app[i][num_level] *= exp(-pow(rx[i][1] - estimate_sum, 2) / (2. * (variance + count * MODIFIER)));
+				}
+
+				if (MODIFIED_LLR)
+				{
+					for (int j = 0; j < NUM_LEVEL; j++)
+					{
+						if (app[i][j] == 0)
+						{
+							flag = false;
+							break;
+						}
+					}
+					if (!flag)
+					{
+						count++;
+						continue;
+					}
+				}
+				//---------- normalization ----------
+				temp = 0;
+				for (int j = 0; j < NUM_LEVEL; j++)
+				{
+					if (app[i][j] < NUMERIC_LIMIT) app[i][j] = NUMERIC_LIMIT;
+					temp += app[i][j];
+				}
+				for (int j = 0; j < NUM_LEVEL; j++)
+				{
+					app[i][j] /= temp;
+					if (app[i][j] < NUMERIC_LIMIT) app[i][j] = NUMERIC_LIMIT;
+				}
+
+
+				if (flag) break;
+			}
+		}
+
+		//---------- a-posteriori LLRs ----------
+		for (int i = 0; i < BLOCK_LEN; i++)
+		{
+			for (int nuser = 0; nuser < NUM_USER; nuser++)
+			{
+				int range = NUM_LEVEL / pow(2, nuser + 1);
+				double app_sum[2] = { 0 };
+				for (int j = 0; j < NUM_LEVEL; j++)
+				{
+					if (j % (2 * range) < range)
+						app_sum[0] += app[i][j];
+					else
+						app_sum[1] += app[i][j];
+				}
+				if (app_sum[0] <= NUMERIC_LIMIT) appLlr[nuser][i] = -LLR_LIMIT;
+				else if (app_sum[1] <= NUMERIC_LIMIT) appLlr[nuser][i] = LLR_LIMIT;
+				else appLlr[nuser][i] = log(app_sum[0] / app_sum[1]);
+			}
+		}
+	}
+	else
+	{
+		int R = 0, CORRENT_USER=0, CORRENT_LEVEL=0, step=0;
+		// R is the bonder of each range
+		// Step is parameter for using channel ceofficient
+
+
+		for (int i = 0; i < known_drift[2 * NUM_USER - 1]; i++)
+		{
+			if (i >= known_drift[R])
+			{
+				R >= NUM_USER ? step++ : step ;
+				R >= NUM_USER ? CORRENT_USER-- : CORRENT_USER++;
+				CORRENT_LEVEL = pow(2, CORRENT_USER);
+				R++;
+			}
+			
+			while (true)
+			{
+				bool flag = true;
+				//---------- APPs ----------
+				for (int num_level = 0; num_level < CORRENT_LEVEL; num_level++)
+				{
+					int reg = num_level;
+					double estimate_sum = 0;
+					for (int j = CORRENT_USER - 1; j >= 0; j--)
+					{
+						if (CE_SCHEME == 1)
+							estimate_sum += pow(-1, (reg % 2)) * chCoef[step + j][0];
+						else
+							estimate_sum += pow(-1, (reg % 2)) * estimate[step + j][0];
+
+						reg /= 2;
+					}
+					app[i][num_level] = exp(-pow(rx[i][0] - estimate_sum, 2) / (2. * (variance + count * MODIFIER)));
+				}
+
+				if (MODIFIED_LLR)
+				{
+					for (int j = 0; j < CORRENT_LEVEL; j++)
+					{
+						if (app[i][j] == 0)
+						{
+							flag = false;
+							break;
+						}
+					}
+					if (!flag)
+					{
+						count++;
+						continue;
+					}
+				}
+				//---------- normalization ----------
+				double temp = 0;
+				for (int j = 0; j < CORRENT_LEVEL; j++)
+				{
+					if (app[i][j] < NUMERIC_LIMIT) app[i][j] = NUMERIC_LIMIT;
+					temp += app[i][j];
+				}
+				for (int j = 0; j < CORRENT_LEVEL; j++)
+				{
+					app[i][j] /= temp;
+					if (app[i][j] < NUMERIC_LIMIT) app[i][j] = NUMERIC_LIMIT;
+				}
+				//---------- APPs ----------
+				for (int num_level = 0; num_level < CORRENT_LEVEL; num_level++)
+				{
+					int reg = num_level;
+					double estimate_sum = 0;
+					for (int j = CORRENT_USER - 1; j >= 0; j--)
+					{
+						if (CE_SCHEME == 1)
+							estimate_sum += pow(-1, (reg % 2)) * chCoef[step + j][1];
+
+						else
+							estimate_sum += pow(-1, (reg % 2)) * estimate[step + j][1];
+
+						reg /= 2;
+					}
+					app[i][num_level] *= exp(-pow(rx[i][1] - estimate_sum, 2) / (2. * (variance + count * MODIFIER)));
+				}
+
+				if (MODIFIED_LLR)
+				{
+					for (int j = 0; j < CORRENT_LEVEL; j++)
+					{
+						if (app[i][j] == 0)
+						{
+							flag = false;
+							break;
+						}
+					}
+					if (!flag)
+					{
+						count++;
+						continue;
+					}
+				}
+				//---------- normalization ----------
+				temp = 0;
+				for (int j = 0; j < CORRENT_LEVEL; j++)
+				{
+					if (app[i][j] < NUMERIC_LIMIT) app[i][j] = NUMERIC_LIMIT;
+					temp += app[i][j];
+				}
+				for (int j = 0; j < CORRENT_LEVEL; j++)
+				{
+					app[i][j] /= temp;
+					if (app[i][j] < NUMERIC_LIMIT) app[i][j] = NUMERIC_LIMIT;
+				}
+				if (flag) break;
+			}
+			//---------- a-posteriori LLRs ----------
+
+			for (int nuser = 0; nuser < CORRENT_USER; nuser++)
+			{
+				int range = CORRENT_LEVEL / pow(2, nuser + 1);
+				double app_sum[2] = { 0 };
+				for (int j = 0; j < CORRENT_LEVEL; j++)
+				{
+					if (j % (2 * range) < range)
+						app_sum[0] += app[i][j];
+					else
+						app_sum[1] += app[i][j];
+				}
+				if (app_sum[0] <= NUMERIC_LIMIT) appLlr[nuser + step][i] = -LLR_LIMIT;
+				else if (app_sum[1] <= NUMERIC_LIMIT) appLlr[nuser + step][i] = LLR_LIMIT;
+				else appLlr[nuser + step][i] = log(app_sum[0] / app_sum[1]);
+			}
+		}
+	}
+}

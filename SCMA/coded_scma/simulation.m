25a7c4cf4e875a8f02a46ab4c9576948dcc76a87
iamsteven526 <r08942052@ntu.edu.tw>
2021-02-19 14:04:56 +0800

    asdf

--- SCMA/coded_scma/simulation.m
+++ SCMA/coded_scma/simulation.m
@@ -43,20 +43,32 @@ K = size(CB, 1); % number of orthogonal resources
 M = size(CB, 2); % number of codewords in each codebook
 V = size(CB, 3); % number of users (layers)
 
-N = 1024; % SCMA signals in frame
+N = 2048; % SCMA signals in frame
 R = 0.5;
-EbN0 = 0:2.5:12.5;
+EbN0 = 15:2.5:15;
 SNR  = EbN0 + 10*log10(R*log2(M)*V/K);   %noise power maybe wrong!!!
 
 Nerr  = zeros(V, length(SNR));
+NBLerr  = zeros(length(SNR));
 Nbits = zeros(V, length(SNR));
+Nblocks = zeros(length(SNR));
 BER   = zeros(V, length(SNR));
+BLER   = zeros(length(SNR));
 
 maxNumErrs = 1000;
-maxNumBits = 5e7;
+maxNumBits = 3e5;
 Niter      = 8;
-ldpcDecoder = comm.LDPCDecoder;
-ldpcEncoder = comm.LDPCEncoder;
+
+fid=fopen('H_2048_1024_z64_0635.txt','r');
+C = textscan(fid,'%d','delimiter',sprintf(' '));
+A = cell2mat(C);
+B = reshape(A,2048,1024)';
+B = sparse(double(B));
+
+ldpcDecoder = comm.LDPCDecoder(B);
+ldpcDecoder.MaximumIterationCount = 250;
+ldpcDecoder.IterationTerminationCondition = 'Parity check satisfied';
+ldpcEncoder = comm.LDPCEncoder(B);
 for k = 1:length(SNR)
 
     N0 = 1/(10^(SNR(k)/10)); % noise power
@@ -68,12 +80,12 @@ for k = 1:length(SNR)
         display(Nbits(1,k))
         dam = randi([0 1], V, N/2); %32400
         for pp = 1:V
-           %w(pp,:) = ldpcEncoder(dam(pp,:)');%64800
-           w(pp,:) = nrPolarEncode(dam(pp,:)',N,10,false);
+           w(pp,:) = ldpcEncoder(dam(pp,:)');%64800
+           %w(pp,:) = nrPolarEncode(dam(pp,:)',N,10,false);
         end
         h = 1/sqrt(2)*(randn(K, V, N)+1j*randn(K, V, N)); % Rayleigh channel
         %h = 1/sqrt(2)*(repmat(randn(1, V, N), K, 1)+1j*repmat(randn(1, V, N), K, 1)); % no diversity
-        multiblock = 8;
+        multiblock = 1;
         for pp = 1:multiblock
             for qq = 1:N/multiblock
                 h(:,:,(pp-1)*N/multiblock+qq) = h(:,:,(pp-1)*N/multiblock+1);
@@ -102,20 +114,26 @@ for k = 1:length(SNR)
         datar   = zeros(log2(M)*N/2, V);
         for kk = 1:V
             for tt = 1:N/2
-                datar(2*tt-1,kk) = LLR(2*kk-1,tt)*N0/1;
-                datar(2*tt,kk) = LLR(2*kk,tt)*N0/1;
+                %datar(2*tt-1,kk) = LLR(2*kk-1,tt)*N0/1;
+                %datar(2*tt,kk) = LLR(2*kk,tt)*N0/1;%tanh(rxDataSoft*NOISE_VAR_1D*4.5).^3)*3
+                datar(2*tt-1,kk) = (tanh(LLR(2*kk-1,tt)*N0*4.5).^1)*5;
+                datar(2*tt,kk) = (tanh(LLR(2*kk,tt)*N0*4.5).^1)*5;
             end%datar(:,kk) = reshape(downsample(datadec, V, kk-1).', [], 1);
         end
         for pp = 1:V
-            ansbit(pp,:) = nrPolarDecode(datar(:,pp),N/2,N,8,10,false,24);
-            %ansbit(pp,:) = ldpcDecoder(datar(:,pp));
+            %ansbit(pp,:) = nrPolarDecode(datar(:,pp),N/2,N,8,10,false,24);
+            ansbit(pp,:) = ldpcDecoder(datar(:,pp));
         end
         err        = sum(xor(dam', ansbit'));
+        NBLerr(k) = NBLerr(:,k) + sum(err>0);
+        Nblocks(k) = Nblocks(k) + 6;
         Nerr(:,k)  = Nerr(:,k) + err.';
         Nbits(:,k) = Nbits(:,k) + log2(M)*N*R;
+        
     end
-    BER(:,k) = Nerr(:,k)./Nbits(:,k);
-    display(sum(Nerr(:,k))/6)
+    BER(:,k) = Nerr(:,k)./(0.5*Nbits(:,k));
+    BLER(k) = NBLerr(k)./Nblocks(k);
+    display(sum(BER(:,k))/6)
     
 end
 plot(EbN0,log10(BER))

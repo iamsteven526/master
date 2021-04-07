49083cf41e308e7053cc0f6c13bd029033f17676
iamsteven526 <r08942052@ntu.edu.tw>
2021-04-07 13:59:40 +0800

    SCMA

--- GDMA/MLDT_uncoded_bpsk_mod2/parameters.h
+++ GDMA/MLDT_uncoded_bpsk_mod2/parameters.h
@@ -3,7 +3,7 @@
 #include <vector>
 //---------- system ----------
 
-#define		NUM_USER				4									// number of users
+#define		NUM_USER				1									// number of users
 #define		NUM_LEVEL				( 1 << NUM_USER )					// number of levels of superimposed signal
 
 #define		DIFF_ENC				0									// differential encoding; 1: enable, 0: disable
@@ -62,7 +62,7 @@
 #define		BLOCK_LEN				2048							// number of symbols in a block
 
 #define		SNR_NUM					5									// number of SNR points to be simulated
-#define		SNR_START				35									// in dB
+#define		SNR_START				15									// in dB
 #define		SNR_STEP			    5									// in dB
 
 #define		HARD(x)					( (x) > 0 ? 0 : 1 )

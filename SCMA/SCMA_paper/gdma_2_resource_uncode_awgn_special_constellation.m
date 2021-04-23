0c54c14d20245ab66b35560d7040539c4707f664
iamsteven526 <r08942052@ntu.edu.tw>
2021-04-23 17:32:54 +0800

    SCMA

--- SCMA/SCMA_paper/gdma_2_resource_uncode_awgn_special_constellation.m
+++ SCMA/SCMA_paper/gdma_2_resource_uncode_awgn_special_constellation.m
@@ -13,17 +13,17 @@ B = sparse(double(B));
 K = 1;
 V = 3; % user
 
-N = 2048; % SCMA signals in frame
+N = 4; % SCMA signals in frame
 M = 2; % BPSK = 2 QPSK = 4 ....
-R = 0.5;  %code rate
-EbN0 = 16:2:20;
+R = 1;  %code rate
+EbN0 = 0:1:5;
 SNR  = EbN0 + 10*log10(2*R*log2(M));
 
 Nerr  = zeros(V, length(SNR));
 Nbits = zeros(V, length(SNR));
 BER   = zeros(V, length(SNR));
 
-maxNumErrs = 100000;
+maxNumErrs = 1000;
 maxNumBits = 6e8;
 Niter      = 5;
 ldpcDecoder = comm.LDPCDecoder(B);
@@ -39,23 +39,26 @@ for k = 1:length(SNR)
         display(k);
         display(Nbits(1,k));
         %x = randi([0 M-1], V, N); % log2(M)-bit symbols
-        dam = randi([0 1], V, N/2); %32400      
-        for pp = 1:V
-           w(pp,:) = ldpcEncoder(dam(pp,:)');%64800
-           %w(pp,:) = nrPolarEncode(dam(pp,:)',N,10,false);
-        end
+        dam = randi([0 1], V, N); %32400      
+%         for pp = 1:V
+%            w(pp,:) = ldpcEncoder(dam(pp,:)');%64800
+%            %w(pp,:) = nrPolarEncode(dam(pp,:)',N,10,false);
+%         end
         
         h = 1/sqrt(2)*(randn(V, N)+1j*randn(V, N)); % Rayleigh channel
         %h = 1/sqrt(2)*(repmat(randn(1, V, N), K, 1)+1j*repmat(randn(1, V, N), K, 1));
-        popo = 4;
+        popo = 1;
         for pp = 1:popo
             for qq = 1:N/popo
-                h(:,(pp-1)*N/popo+qq) = h(:,(pp-1)*N/popo+1);
+                %h(:,(pp-1)*N/popo+qq) = h(:,(pp-1)*N/popo+1);
+                h(1,:) = 1;
+                h(2,:) = 1j;
+                h(3,:) = 0.9719 + 0.2354j;
             end
         end
         for pp = 1:V
            for tt = 1:N/log2(M)
-               x(pp,tt) = 1 - 2*w(pp,tt);
+               x(pp,tt) = 1 - 2*dam(pp,tt);
                %x(pp,tt) = w(pp,2*tt) + 2*w(pp,2*tt-1); %ex:10 = 2 01 = 1 %32400
            end
         end
@@ -65,15 +68,18 @@ for k = 1:length(SNR)
         LLR = scmadec777(y, h, N0);
         LLR(LLR==inf) = 1500;
         LLR(LLR==-inf) = -1500;
+        
+        LLR(LLR >= 0) = 0;
+        LLR(LLR < 0) = 1;
         % symbol to bit conversion
         %r    = de2bi(x, log2(M), 'left-msb');
 
-        for pp = 1:V
-            %ansbit(pp,:) = nrPolarDecode(datar(:,pp),N/2,N,8,10,false,24);
-            ansbit(pp,:) = ldpcDecoder(LLR(pp,:)');
-        end
+%         for pp = 1:V
+%             %ansbit(pp,:) = nrPolarDecode(datar(:,pp),N/2,N,8,10,false,24);
+%             ansbit(pp,:) = ldpcDecoder(LLR(pp,:)');
+%         end
 
-        err        = sum(xor(dam', ansbit'));
+        err        = sum(xor(dam', LLR'));
         Nerr(:,k)  = Nerr(:,k) + err.';
         Nbits(:,k) = Nbits(:,k) + log2(M)*N*R;
     end

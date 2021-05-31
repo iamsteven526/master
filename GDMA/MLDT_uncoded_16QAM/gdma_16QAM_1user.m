31ba20aa0ccceb7e1af62efbd64b81a0c913cd29
iamsteven526 <r08942052@ntu.edu.tw>
2021-05-31 10:27:23 +0800

    SCMA

---
+++ GDMA/MLDT_uncoded_16QAM/gdma_16QAM_1user.m
@@ -0,0 +1,65 @@
+%3 user BPSK + LDPC
+
+clc;
+clear;
+
+K = 1;
+V = 1; % user
+
+N = 20; % SCMA signals in frame
+M = 16; % BPSK = 2 QPSK = 4 ....
+R = 1;  %code rate
+EbN0 = 0:5:40;
+SNR  = EbN0 + 10*log10(R*log2(M));
+
+Nerr  = zeros(V, length(SNR));
+Nbits = zeros(V, length(SNR));
+BER   = zeros(V, length(SNR));
+
+maxNumErrs = 10000;
+maxNumBits = 6e8;
+Niter      = 5;
+
+for k = 1:length(SNR)
+
+    N0 = 1/(10^(SNR(k)/10)); % noise power
+
+    while ((min(Nerr(:,k)) < maxNumErrs) && (Nbits(1,k) < maxNumBits))
+        display(k);
+        display(Nbits(1,k));
+        %x = randi([0 M-1], V, N); % log2(M)-bit symbols
+        w = randi([0 1], V, N); %32400      
+
+        
+        h = 1/sqrt(2)*(randn(V, N/4)+1j*randn(V, N/4)); % Rayleigh channel
+        popo = 1;
+        for pp = 1:popo
+            for qq = 1:N/(popo*4)
+                h(:,(pp-1)*N/(popo*4)+qq) = h(:,(pp-1)*N/(popo*4)+1);
+            end
+        end
+
+        for pp = 1:V
+           x(pp,:) = qammod(w(pp,:)',16,'UnitAveragePower',true,'InputType','bit')';
+        end
+        s = sum(h.*x,1); % joint encoding and fading channel propagation
+        y = awgn(s, SNR(k));
+
+        LLR = scmadec777_1user(y, h, N0);
+        LLR(LLR==inf) = 1500;
+        LLR(LLR==-inf) = -1500;       
+        LLR(LLR>= 0) = 0;
+        LLR(LLR< 0) = 1;
+        
+        % symbol to bit conversion
+        %r    = de2bi(x, log2(M), 'left-msb');
+
+        err        = sum(xor(w', LLR'));
+        Nerr(:,k)  = Nerr(:,k) + err.';
+        Nbits(:,k) = Nbits(:,k) + N;
+    end
+    BER(:,k) = Nerr(:,k)./Nbits(:,k);
+    k
+    display(sum(Nerr(:,k))/1)
+end
+plot(EbN0,log10(BER))
---
+++ GDMA/MLDT_uncoded_16QAM/scmadec777_2user.m
@@ -0,0 +1,50 @@
+function LLR = scmadec777_3user(y, h, N0)
+
+M = 16;
+V = 2;
+
+% Factor graph calculation
+
+N   = size(y, 2);
+LLR = zeros(V, log2(M)*N);
+
+Noise = 1/N0;
+
+QAM_point = qammod([0:15],16,'UnitAveragePower',true,'InputType','integer')';
+
+for jj = 1:N
+
+    % Step 1: Initial calculations
+    f = zeros(M, M);
+    
+     % non-zero elements, paths
+    for m1 = 1:M
+        for m2 = 1:M
+            d = y(jj)-(QAM_point(m1)*h(1,jj)+QAM_point(m2)*h(2,jj));
+            f(m1,m2) = exp(-Noise*sum(real(d)^2+imag(d)^2));
+        end
+    end
+
+% Step 3: LLR calculation
+    Q = zeros(M, V);
+    for mm = 1:M
+        Q(mm,1) = sum(sum(f(mm,:)));
+        Q(mm,2) = sum(sum(f(:,mm)));
+    end
+    %Q(1,1) = sum(sum(f(1,:,:)));
+    %Q(2,1) = sum(sum(f(2,:,:)));
+    %Q(1,2) = sum(sum(f(:,1,:)));
+    %Q(2,2) = sum(sum(f(:,2,:)));    
+    %Q(1,3) = sum(sum(f(:,:,1)));
+    %Q(2,3) = sum(sum(f(:,:,2)));
+
+    LLR_tmp = zeros(V, log2(M)*1); % temp variable for parallel work
+
+    for k = 1:V
+        LLR_tmp(k,1) = log(sum(Q(1:8,k))/sum(Q(9:16,k)));
+        LLR_tmp(k,2) = log((sum(Q(1:4,k))+sum(Q(9:12,k)))/(sum(Q(5:8,k))+sum(Q(13:16,k))));
+        LLR_tmp(k,3) = log((sum(Q(1:4:13,k))+sum(Q(2:4:14,k)))/(sum(Q(3:4:15,k))+sum(Q(4:4:16,k))));
+        LLR_tmp(k,4) = log(sum(Q(1:2:15,k))/sum(Q(2:2:16,k)));
+    end
+    LLR(:,4*(jj-1)+1:4*jj) = LLR_tmp;
+end
